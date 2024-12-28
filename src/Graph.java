88ae2071d8ca148f3e58b2d46d8e72cd7a7730dc
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2024-12-28 20:10:38 +0100

    feat: added Graph class

---
+++ src/Graph.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Graph {
+    private List<Node> nodes;
+    private List<Edge> edges;
+
+    public Graph(List<Node> nodes,List<Edge> edges){
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public List<Node> getNodes(){
+        return nodes;
+    }
+    public List<Edge> getEdges(){
+        return edges;
+    }
+
+    public void addEdge(Edge edge){
+        edges.add(edge);
+    }
+
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+
+    public void removeEdge(Edge edge){
+        edges.remove(edge);
+    }
+
+    public void removeNode(Node node){
+        nodes.remove(node);
+    }
+
+    @Override
+    public String toString(){
+        return "Nodes:"+nodes;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Node nodeA = new Node("A");
         Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
 
-        Edge edge = new Edge(nodeA,nodeB);
-        for (Node i: edge.getNodes()){
+        Edge edge1 = new Edge(nodeA,nodeB);
+        Edge edge2 = new Edge(nodeB,nodeC);
+
+        for (Node i: edge1.getNodes()){
             System.out.println(i+" ");
         }
+
+        Node[] nodeArr = {nodeA,nodeB,nodeC};
+        Edge[] edgeArr = {edge1,edge2};
+
+        Graph graph = new Graph(Arrays.asList(nodeArr),Arrays.asList(edgeArr));
+        System.out.println(graph);
     }
-    
 }
