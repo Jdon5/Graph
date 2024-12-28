63d89ef073e8ff3c757f72a60c9e7b4ef67f71ac
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2024-12-28 20:38:44 +0100

    feat: added WeigthedEdge class

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,16 @@ public class Main {
         Node nodeB = new Node("B");
         Node nodeC = new Node("C");
         Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
 
         Edge edge1 = new Edge(nodeA,nodeB);
         Edge edge2 = new Edge(nodeB,nodeC);
         DirectedEdge edge3 = new DirectedEdge(nodeC, nodeD);
+        WeightedEdge edge4 = new WeightedEdge(nodeD, nodeE, 2);
 
 
         Node[] nodeArr = {nodeA,nodeB,nodeC,nodeD};
-        Edge[] edgeArr = {edge1,edge2,edge3};
+        Edge[] edgeArr = {edge1,edge2,edge3,edge4};
 
 
         Graph graph = new Graph(Arrays.asList(nodeArr),Arrays.asList(edgeArr));
---
+++ src/WeightedEdge.java
@@ -0,0 +1,18 @@
+public class WeightedEdge extends Edge{
+
+    private double weight;
+
+    public WeightedEdge(Node firstNode, Node secondNode, double weight){
+        super(firstNode,secondNode);
+        this.weight = weight;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    @Override
+    public String toString(){
+        return getFirstNode()+" -("+weight+")- "+getSecondNode();
+    }
+}
